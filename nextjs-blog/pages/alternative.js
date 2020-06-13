670129901a2ee815306f32f0c46f4f882759c6e4
Aashish Ananthanarayan <aashish.2704@tamu.edu>
2020-06-13 09:48:01 -0500

    The first template of the web browser

---
+++ nextjs-blog/pages/alternative.js
@@ -0,0 +1,1 @@
+export default function Alternative() {   return <h1>This is the page to post alternative forms of aid</h1> }
--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -10,39 +10,39 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          United Pantry
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Whether you need help or want to help, we want to be there.
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="/offerings" className="card">
+            <h3>Offerings near me &rarr;</h3>
+            <p>Find offerings near you catered to your needs.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="/post" className="card">
+            <h3>Post an offering &rarr;</h3>
+            <p>Offer help to those in need, anything helps.</p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="/filters"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Filters &rarr;</h3>
+            <p>Narrow down results based on your preferences.</p>
+          </a>
+
+          <a
+            href="/alternative"
+            className="card"
+          >
+            <h3>Community Help &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Find or give alternative forms of aid to the community.
             </p>
           </a>
         </div>
@@ -50,12 +50,12 @@ export default function Home() {
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/theananthanaryan/summer_hacks"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          Powered by Aggie Engineers
+         
         </a>
       </footer>
 
